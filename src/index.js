ada1823919c4a4f223d74609b97614f7a51d4571
Lucie Caillat <lucie.caillat@gmail.com>
2023-07-07 11:21:57 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import Modal from "./lib/Modal";
-export { Modal };
-
-ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
+const lib = { Modal };
+export default lib;
